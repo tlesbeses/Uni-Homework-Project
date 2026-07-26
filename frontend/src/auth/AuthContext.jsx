9d135caf72a3d7dd1776d9a170217386fb7ff9d4
Pevin <pevin.kevin@gmail.com>
2026-07-25 18:14:30 -0600

    feat: add AuthContext and AuthProvider for user authentication state management

---
+++ frontend/src/auth/AuthContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext(null);
+
+
+export function AuthProvider({ children }) {
+
+    const [user, setUser] = useState(null);
+
+
+    return (
+        <AuthContext.Provider
+            value={{
+                user,
+                setUser
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    )
+}
