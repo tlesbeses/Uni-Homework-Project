0e8dee9efe12ddc15c3b823961994cae2c285401
Pevin <pevin.kevin@gmail.com>
2026-07-27 00:53:44 -0600

    feat: add django-filter and update settings for auth and users apps

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auth',
+    'users',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    "django_filters",
 ]
 
 MIDDLEWARE = [
