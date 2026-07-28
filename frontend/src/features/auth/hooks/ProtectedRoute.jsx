13e367803b0b768587a94a4ca22ab2a60eb219ba
Pevin <pevin.kevin@gmail.com>
2026-07-28 16:35:05 -0600

    feat: implement authentication context and protected route; add Navbar component

--- frontend/src/features/auth/hooks/ProtectedRoute.jsx
+++ frontend/src/features/auth/hooks/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../auth/context/AuthProvider.jsx";
+import { useAuth } from "../providers/AuthProvider.jsx";
 
 export default function ProtectedRoute({
     children,
