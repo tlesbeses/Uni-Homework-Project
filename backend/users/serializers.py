52b9fe1242ee1a254f39d2c66290dfaa51191491
Pevin <pevin.kevin@gmail.com>
2026-07-28 13:22:24 -0600

    feat: add LoginUserSerializer and enhance LoginSerializer for user roles and permissions

---
+++ backend/users/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class LoginUserSerializer(serializers.ModelSerializer):
+    roles = serializers.SerializerMethodField()
+    permissions = serializers.SerializerMethodField()
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "username",
+            "roles",
+            "permissions",
+        )
+
+    def get_roles(self, obj):
+        return list(obj.groups.values_list("name", flat=True))
+
+    def get_permissions(self, obj):
+        return list(obj.get_all_permissions())
+
+
+class LoginSerializer(TokenObtainPairSerializer):
+
+    @classmethod
+    def get_token(cls, user):
+        return super().get_token(user)
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data["user"] = LoginUserSerializer(self.user).data
+
+        return data
