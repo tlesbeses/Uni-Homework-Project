707e59c42ad79d49ab1737e1056ffc9dedea5c1f
Pevin <pevin.kevin@gmail.com>
2026-07-30 00:32:27 -0600

    feat: add initial migration files for authentication and create default user groups

---
+++ backend/authentication/migrations/0002_create_default_groups.py
@@ -0,0 +1,18 @@
+from django.db import migrations
+
+def create_groups(apps, schema_editor):
+    Group = apps.get_model("auth", "Group")
+
+    Group.objects.get_or_create(name="Student")
+    Group.objects.get_or_create(name="Teacher")
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("authentication", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.RunPython(create_groups),
+    ]
